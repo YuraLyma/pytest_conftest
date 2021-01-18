2fc546b335f0dbff4df80e50003a9228774dcc78
yura.lyma@gmail.com <yura.lyma@gmail.com>
2021-01-18 16:04:57 +0200

    changing time.sleep(3) to time.sleep(30)

--- test_items.py
+++ test_items.py
@@ -8,7 +8,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_to_baskett(browser):
 	browser.get(link)
-	time.sleep(3)
+	time.sleep(30)
 	try:
 		btn = browser.find_element_by_css_selector(".btn-add-to-basket")
 	except NoSuchElementException:
