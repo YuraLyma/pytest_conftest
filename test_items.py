70385aae7bf801d5eb1e695809b55eab247a48fd
yura.lyma@gmail.com <yura.lyma@gmail.com>
2021-01-18 16:02:39 +0200

    "add to basket" button test

---
+++ test_items.py
@@ -0,0 +1,17 @@
+import time
+
+from selenium.common.exceptions import NoSuchElementException
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_add_to_baskett(browser):
+	browser.get(link)
+	time.sleep(3)
+	try:
+		btn = browser.find_element_by_css_selector(".btn-add-to-basket")
+	except NoSuchElementException:
+		btn = False
+
+	assert btn, 'There are no "Add to basket" buttons'
